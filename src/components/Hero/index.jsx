b6760c58bfc2698e9c59158d9dd70cb73d45ea27
Erick Nascimento <erick.nascim3nto@gmail.com>
2023-05-22 16:12:26 -0300

    fix: removing comments

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,12 +3,6 @@ import Herocontent from '../Hero/HeroContent/index'
 
 function Hero() {
   return (
-    /**
-     * 
-     * <div className="mt-[-80px] w-full z-40 h-[600px] object-cover bg-[url('./src/assets/stranger.jpg')] bg-no-repeat bg-cover">
-      <p>Teste</p>
-    </div>
-    */
     <div className="relative">
       <img
         src={HeroImg}
